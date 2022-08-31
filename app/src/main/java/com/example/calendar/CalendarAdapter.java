9d815a5d350c312729c50df5fe2a3354c9619ebc
giakhanh2711 <53686667+giakhanh2711@users.noreply.github.com>
2022-08-31 14:43:48 +0700

    Check chọn ngày valid, tô đỏ ngày hiện tại

--- app/src/main/java/com/example/calendar/CalendarAdapter.java
+++ app/src/main/java/com/example/calendar/CalendarAdapter.java
@@ -1,15 +1,24 @@
 package com.example.calendar;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.finalproject.Helper;
 import com.example.finalproject.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder>
 {
@@ -33,6 +42,17 @@ public class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder>
 
     @Override
     public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position) {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat getMonthYear = new SimpleDateFormat("MMMM yyyy");
+        String monthYearCurrent = getMonthYear.format(c.getTime());
+
+        SimpleDateFormat getDay = new SimpleDateFormat("dd");
+        String dayCurrent = getDay.format(c.getTime());
+
+        if (listOfDays.get(position).getDayOfMonth().equals(dayCurrent) && monthYearCurrent.equals(CalendarFragment.monthYear)){
+            holder.dayOfMonth.setTextColor(Color.parseColor("#FD0A5A"));
+        }
+
         holder.dayOfMonth.setText(listOfDays.get(position).getDayOfMonth());
         holder.llCell.setOnClickListener(v -> onItemListener.onItemClick(position));
     }
